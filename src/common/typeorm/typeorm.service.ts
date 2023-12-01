f9b27003e0da097c70245c556766fe32bec249aa
Pavlo Antosiak <pavlo.antosiak@gmail.com>
2023-12-01 10:46:48 +0200

    Containerizing an app

--- src/common/typeorm/typeorm.service.ts
+++ src/common/typeorm/typeorm.service.ts
@@ -12,11 +12,11 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   public createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: this.config.get<string>('DATABASE_HOST'),
-      port: this.config.get<number>('DATABASE_PORT'),
-      database: this.config.get<string>('DATABASE_NAME'),
-      username: this.config.get<string>('DATABASE_USER'),
-      password: this.config.get<string>('DATABASE_PASSWORD'),
+      host: this.config.get<string>('PG_DATABASE_HOST'),
+      port: this.config.get<number>('PG_DATABASE_PORT'),
+      database: this.config.get<string>('PG_DATABASE_NAME'),
+      username: this.config.get<string>('PG_DATABASE_USER'),
+      password: this.config.get<string>('PG_DATABASE_PASSWORD'),
       entities: ['dist/**/*.entity.{ts,js}'],
       migrations: ['dist/migrations/*.{ts,js}'],
       migrationsTableName: 'typeorm_migrations',
